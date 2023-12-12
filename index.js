ce76df2b95e1818060a4e644a2166763f72f96a9
quill_nichols <leplep00100@gmail.com>
2023-12-12 14:57:16 -0500

    neat

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+const mondayWork = function(activity = "go to the office") {
+    return  `This Monday, I will ${activity}.`;
+}
+function wrapAdjective(symbol = "*") {
+    return function(adjective = 'a hard worker') {
+        return `You are ${symbol}${adjective}${symbol}!`;
+    }
+}
 // code your solution here
